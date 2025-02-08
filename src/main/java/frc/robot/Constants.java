0bc91680d712742783085edd73d3d90328f45cef
Hugo Wang <hwang@greenhillsschool.org>
2025-02-08 20:22:06 +0000

    fix constants changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -134,14 +134,14 @@ public final class Constants {
 
 
 	}
-	
+
 	public static final class PivotConstants {
-		
+
 		public static final int pivotId = 0;
 		public static final int intakeId = 1;
 		public static final int laserCan1Id = 1;
 		public static final int laserCan2Id = 2;
-		
+
 		public static final double intakePower = 0.3;
 
 		public static final double Kp = 0.01;
@@ -155,7 +155,7 @@ public final class Constants {
 
 		//Feedforward Calculations
 		public static final double length = 0;
-		
+
 		public static final double restistance = 1;
 		public static final double mass = 2;
 		public static final double gravity = 9.81;
@@ -165,108 +165,5 @@ public final class Constants {
 		public static final double kt = (105-1.8) / 2.6;
 
 
-	}
-	public static final class SwerveConstants {
-		public static final double trackWidth = Units.inchesToMeters(23);
-		public static final double wheelBase = Units.inchesToMeters(23);
-
-		public static final class FrontLeftModule { // FL
-			public static final int driveMotor = 5;
-			public static final int turnMotor = 6;
-			public static final int canCoder = 13;
-			public static final double angleOffset = 0;
-		}
-
-		public static final class FrontRightModule { // FR
-			public static final int driveMotor = 7;
-			public static final int turnMotor = 8;
-			public static final int canCoder = 14;
-			public static final double angleOffset = 0;
-		}
-
-		public static final class RearRightModule { // RR
-			public static final int driveMotor = 9;
-			public static final int turnMotor = 10;
-			public static final int canCoder = 15;
-			public static final double angleOffset =0;
-		}
-
-		public static final class RearLeftModule { // RL
-			public static final int driveMotor = 11;
-			public static final int turnMotor = 12;
-			public static final int canCoder = 16;
-			public static final double angleOffset = 0;
-		}
-
-		public static final class SwerveModuleConstants {
-			public static final double conversionFactor = (1 / 6.75) * Units.inchesToMeters(Math.PI * 4);
-
-			/**
-			 * number to multiply
-			 * rotations
-			 * by to
-			 * find meters of movement, specifically rpm to m/min conversion based on wheel size
-			 */
-
-			public static final class SwerveAnglePIDConstants {
-				public static final double p = 0.3;
-				public static final double i = 0;
-				public static final double d = 0;
-			}
-		}
-
-		public static final class RotationConstants {
-			public static final double kP = 0.1;
-			public static final double kI = 0.0;
-			public static final double kD = 0.0;
-
-			public static final double controllerTolerance = 0.1; // Arbitrary PIDController tolerance to determine when
-																	// setpoint is reached. Relevant for knowing when
-																	// rotation based commands finish
-		}
-
-		public static final class PathPlannerConstants {
-			public static final double driveBaseRadius = Units
-					.inchesToMeters(Math.sqrt(trackWidth * trackWidth + wheelBase * wheelBase));
-
-			public static final PIDConstants translationConstants = new PIDConstants(5, 0.5, 0);
-			public static final PIDConstants rotationConstants = new PIDConstants(3, 0, 0);
-
-			public static final PathConstraints constraints = new PathConstraints(1, 1, 1, 1);
-		}
-
-		public static final Translation2d front_left = new Translation2d(Constants.SwerveConstants.trackWidth / 2,
-				Constants.SwerveConstants.wheelBase / 2);
-		public static final Translation2d front_right = new Translation2d(Constants.SwerveConstants.trackWidth / 2,
-				-Constants.SwerveConstants.wheelBase / 2);
-		public static final Translation2d rear_left = new Translation2d(-Constants.SwerveConstants.trackWidth / 2,
-				Constants.SwerveConstants.wheelBase / 2);
-		public static final Translation2d rear_right = new Translation2d(-Constants.SwerveConstants.trackWidth / 2,
-				-Constants.SwerveConstants.wheelBase / 2); //RR
-		public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(front_left, front_right, rear_right, rear_left);
-	}
-
-	public static final class ControllerConstants {
-		public static final int driveControllerPort = 0;
-		public static final double driveControllerJoystickDeadband = 0.06;
-
-		public static final int secondaryControllerPort = 1;
-		public static final double secondaryControllerJoystickDeadband = 0.1;
-	}
-
-	public static final class VisionTargeterConstants {
-		public static final double kP = 0.1;
-		public static final double kI = 0.1;
-		public static final double kD = 0.1;
-
-		public static final double taMultiplier = 1.25;
-	}
-
-	public static final class GyroConstants {
-		public static final int pigeonPort = 17;
-	}
-
-	public static final class LimelightConstants {
-		public static final String primaryTableKeyName = "limelight";
 	}
 }
