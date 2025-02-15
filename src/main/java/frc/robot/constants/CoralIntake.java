e67ce86fe1f0b2a68af16142a4c95568cb5fe51e
Hugo Wang <hwang@greenhillsschool.org>
2025-02-15 12:08:08 -0500

    post rebase fixes

--- src/main/java/frc/robot/constants/CoralIntake.java
+++ src/main/java/frc/robot/constants/CoralIntake.java
@@ -2,20 +2,20 @@ package frc.robot.constants;
 
 public class CoralIntake {
     public static class Pivot {
-        public static final int pivotId = 0;
-        public static final int intakeId = 6;
+        public static final int pivotId = 6;
+        public static final int intakeId = 0;
         public static final int laserCan1Id = 42;
         public static final int laserCan2Id = 0;
 
         public static final double intakePower = -0.6;
 
-        public static final double Kp = 0.01;
+        public static final double Kp = 1.65;
 
         //Targets (Degrees)
-        public static final double intakePos = 0;
-        public static final double middlePos = 0;
-        public static final double topPos = 0;
-        public static final double bottomPos = 0;
+        public static final double intakePos = 0.712;
+        public static final double middlePos = 0.712;
+        public static final double topPos = 0.641;
+        public static final double bottomPos = 0.858;
 
 
         //Feedforward Calculations
@@ -25,9 +25,8 @@ public class CoralIntake {
         public static final double mass = 2;
         public static final double gravity = 9.81;
         //Inverse
-        public static final double center = 1/2.0;
-        public static final double gearRatio  = 1/36.0;
-        public static final double kt = (105-1.8) / 2.6;
+        public static final double center = 1 / 2.0;
+        public static final double gearRatio = 1 / 36.0;
+        public static final double kt = (105 - 1.8) / 2.6;
     }
 }
-
