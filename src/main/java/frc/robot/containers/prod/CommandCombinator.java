5ed060beb7c426b6424d6d4c92b8f1e1ac1ad056
naackisnotavailable <108686834+naackisnotavailable@users.noreply.github.com>
2025-02-15 14:12:50 -0500

    fix import in prod container

--- src/main/java/frc/robot/containers/prod/CommandCombinator.java
+++ src/main/java/frc/robot/containers/prod/CommandCombinator.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import frc.robot.Robot;
+import frc.robot.containers.prod.RobotContainer.Subsystems;
 
 /**
  * Class for combining commands defined in {@link RobotContainer}. Efficent way
