a231d5eda3ce10e30f8d7beba429fe4984769771
Adhrit Bharath <abharath@greenhillsschool.org>
2025-02-21 17:26:00 -0500

    fish

---
+++ src/main/java/frc/robot/constants/FisheyeCameraConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public class FisheyeCameraConstants {
+    public String name = "HangCamera";
+    public int port = 0; 
+    
+    public int width = 1280;
+    public int height = 720;
+    public int fps = 30;
+
+}
