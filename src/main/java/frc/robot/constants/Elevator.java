e601f5b39eb0dd6bbce4375c8310db64b306a684
naackisnotavailable <108686834+naackisnotavailable@users.noreply.github.com>
2025-03-03 09:19:42 -0500

    speed up elevator

--- src/main/java/frc/robot/constants/Elevator.java
+++ src/main/java/frc/robot/constants/Elevator.java
@@ -7,8 +7,8 @@ public class Elevator {
     public static final double level1 = 14; //unsure of actual value was 28
     public static final double intake = 0.1;
 
-    public static final double minSpeed = -0.6;
-    public static final double maxSpeed = 0.85;
+    public static final double minSpeed = -0.8;
+    public static final double maxSpeed = 1;
 
     public static final double kP = 0.033;
     public static final double kI = 0.000;
